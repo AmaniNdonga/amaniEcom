8041bae7792e64e07ae122928721bfff81829581
Abraham Amani Ndonga <amaniabraham0@gmail.com>
2019-10-22 11:46:34 -0400

    second commit ignored node modules as well

---
+++ src/components/auth/signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+
+  render() {
+      return (
+          <div className="sign-up">
+              <p>sign uppp</p>
+          </div>
+        )
+  }
+}
+
+export default Signup;
---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from "./headernavbar/header";
+import Navbar from "./headernavbar/navbar";
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className='layout'>
+        
+        <Header/>
+        <Navbar/>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+export default Layout;
